1b23f6f16914eeb012031080002c4d8732767ab7
MojtabaDwiek <jakodwk717@gmail.com>
2025-11-25 16:59:33 +0200

    header fix

--- script.js
+++ script.js
@@ -246,10 +246,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const header = document.querySelector(".header");
 
         if (window.scrollY > 50) {
-            header.style.background = "rgba(15, 23, 42, 0.95)";
+            header.style.background = "linear-gradient(90deg, var(--primary), var(--secondary))";
             header.style.boxShadow = "0 5px 20px rgba(0, 0, 0, 0.15)";
         } else {
-            header.style.background = "rgba(15, 23, 42, 0.9)";
+            header.style.background = "linear-gradient(90deg, var(--primary), var(--secondary))";
             header.style.boxShadow = "none";
         }
 
